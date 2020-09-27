13ed9727cca8876d7e1f75d1f5793591dab0dcb2
Uci <sef.gustingurah.yama@gmail.com>
2020-09-27 09:04:21 +0700

    edit UI and add functionality

--- Labx/app/src/main/java/id/ac/ui/cs/mobileprogramming/yama/caloriemeter/MainActivity.java
+++ Labx/app/src/main/java/id/ac/ui/cs/mobileprogramming/yama/caloriemeter/MainActivity.java
@@ -2,13 +2,41 @@ package id.ac.ui.cs.mobileprogramming.yama.caloriemeter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private int counter = 0;
+    private TextView showCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void countDown(View view) {
+        counter--;
+        showCount.setText(Integer.toString(counter));
+    }
+
+    public void countUp(View view) {
+        counter++;
+        showCount.setText(Integer.toString(counter));
+    }
+
+    public void countReset(View view) {
+        counter = 0;
+        showCount.setText(Integer.toString(counter));
+
+        Random rnd = new Random();
+        int color = Color.argb(255, rnd.nextInt(256),
+                rnd.nextInt(256), rnd.nextInt(256));
+        showCount.setBackgroundColor(color);
     }
 }
