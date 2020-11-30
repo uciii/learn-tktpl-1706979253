349bdf57c76185792837f33cc9b542a62887723a
uciii <sef.gustingurah.yama@gmail.com>
2020-11-30 10:00:47 +0700

    complete sending post request to https

--- Labx/app/src/main/java/id/ac/ui/cs/mobileprogramming/yama/caloriemeter/MainActivity.java
+++ Labx/app/src/main/java/id/ac/ui/cs/mobileprogramming/yama/caloriemeter/MainActivity.java
@@ -20,7 +20,17 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     ListAdapter listAdapter;
     ListView wifiListView;
     Button refresh, send;
-    TextView response;
+    TextView responseV;
     List wifiList;
 
     @SuppressLint("WifiManagerLeak")
@@ -41,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         wifiListView = findViewById(R.id.list_item);
         refresh = findViewById(R.id.refresh);
         send = findViewById(R.id.send);
-        response = findViewById(R.id.response);
+        responseV = findViewById(R.id.response);
 
         wifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
         wifiReciever = new WifiReciever();
@@ -52,6 +62,58 @@ public class MainActivity extends AppCompatActivity {
                 showWifi();
             }
         });
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest();
+            }
+        });
+    }
+
+    private void sendRequest() {
+        if(ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 0);
+        }
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://570952b4df932d54fdbeb7879af2acf5.m.pipedream.net";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        responseV.setText("Response is: "+ response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        responseV.setText("That didn't work!");
+                    }
+                }){
+                @Override
+                protected Map<String,String> getParams(){
+                    Map<String,String> params = new HashMap<String, String>();
+                    params.put("wifi_list",wifiList.toString());
+                    return params;
+                }
+
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<String, String>();
+                    params.put("Content-Type","application/x-www-form-urlencoded");
+                    return params;
+                }
+        };
+
+        queue.add(stringRequest);
+
     }
 
     private void showWifi(){
